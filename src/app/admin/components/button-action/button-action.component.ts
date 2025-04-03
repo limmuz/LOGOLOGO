cf84696d8c8701043bb54e104e25be33a2c6e1af
Nicole <nicolelinscoelho@gmail.com>
2025-04-02 22:09:28 -0300

    Pagina de manutenção iniciada

---
+++ src/app/admin/components/button-action/button-action.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-action',
+  imports: [],
+  templateUrl: './button-action.component.html',
+  styleUrl: './button-action.component.css'
+})
+export class ButtonActionComponent {
+  @Input() textBtn:string = ""
+  @Input() cor:string = ""
+}
--- src/app/admin/pages/home-admin/home-admin.component.ts
+++ src/app/admin/pages/home-admin/home-admin.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from "../../components/admin-layout/admin-layout.component";
+import { ButtonActionComponent } from "../../components/button-action/button-action.component";
 
 @Component({
   selector: 'app-home-admin',
-  imports: [RouterModule, AdminLayoutComponent],
+  imports: [RouterModule, AdminLayoutComponent, ButtonActionComponent],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.css'
 })
 export class HomeAdminComponent {
-
+  @Input() nome: string = ""
+  @Input() email: string = ""
 }
