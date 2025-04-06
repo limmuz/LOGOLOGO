a12acc320de74d352ad860934d3d7a66552b9423
Nicole Lins <nicolelinscoelho@gmail.com>
2025-04-06 01:53:52 -0300

    Conflitos arrumados

--- src/app/admin/pages/home-admin/home-admin.component.ts
+++ src/app/admin/pages/home-admin/home-admin.component.ts
@@ -1,12 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AdminLayoutComponent } from "../../components/admin-layout/admin-layout.component";
-import { ButtonActionComponent } from "../../components/button-action/button-action.component";
 import { TableComponent } from "../../components/table/table.component";
 
 @Component({
   selector: 'app-home-admin',
-  imports: [RouterModule, AdminLayoutComponent, ButtonActionComponent, TableComponent],
+  imports: [RouterModule, TableComponent],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.css'
 })
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 import { UnderConstructionsComponent } from "../../components/under-constructions/under-constructions.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [HeaderComponent, FooterComponent, UnderConstructionsComponent, RouterModule],
+  imports: [UnderConstructionsComponent, RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
