2b4c5b8e9118046dab84291c6f02e99da9e8aa8a
Nicole <nicolelinscoelho@gmail.com>
2025-04-09 22:00:55 -0300

    Pagina de logout admin inicio

--- src/app/admin/pages/log-out-admin/log-out-admin.component.ts
+++ src/app/admin/pages/log-out-admin/log-out-admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AdminLayoutComponent } from "../../components/admin-layout/admin-layout.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-log-out-admin',
-  imports: [AdminLayoutComponent],
+  imports: [AdminLayoutComponent, RouterModule],
   templateUrl: './log-out-admin.component.html',
   styleUrl: './log-out-admin.component.css'
 })
