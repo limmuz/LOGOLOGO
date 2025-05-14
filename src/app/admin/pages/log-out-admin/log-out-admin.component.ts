ff55f694291e46e7e47516dff48b85db54f8dc38
zummil <miguellima.nascimento.005@gmail.com>
2025-05-14 20:31:50 -0300

    Update log-out-admin.component.ts

--- src/app/admin/pages/log-out-admin/log-out-admin.component.ts
+++ src/app/admin/pages/log-out-admin/log-out-admin.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AdminLayoutComponent } from "../../components/admin-layout/admin-layout.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-log-out-admin',
+  standalone: true,  
   imports: [AdminLayoutComponent, RouterModule],
   templateUrl: './log-out-admin.component.html',
   styleUrl: './log-out-admin.component.css'
 })
 export class LogOutAdminComponent {
 
+  constructor(private router: Router) {}
+
+  logout() {
+    
+    console.log('Executando logout...');
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userData');
+    this.router.navigate(['/login']);
+  }
 }
