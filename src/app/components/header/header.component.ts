27df58d96849472f7c6cd54ff59097cd2a10184e
Nicole Lins <nicolelinscoelho@gmail.com>
2025-05-18 16:15:31 -0300

    Warnings retirados

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LogoComponent } from "../logo/logo.component";
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-header',
